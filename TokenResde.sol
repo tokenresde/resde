1d518528d2e18e26b4ed3738466a6b052e17b251
tokenresde <tokenresde@gmail.com>
2024-05-30 11:28:10 -0300

    Update TokenResde.sol

--- TokenResde.sol
+++ TokenResde.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.8.2;
+
 contract Token {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
@@ -7,36 +8,32 @@ contract Token {
     string public symbol = "rsd;
     uint public decimals = 18;
     
-     require(balanceOf(from) >= value, 'balance too low');
-     require(allowance[from][msg.sender] >= value, 'allowance too low');
-
-     constructor() {
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor() {
         balances[msg.sender] = totalSupply;
-        
-        function balancreturn balances[owner];eOf(address owner) public 
-        returns(uint) {
-                
-        balances[msg.sender] -= value;
-       emit Transfer(msg.sender, to, value);
-        return true;
     }
     
-          
-        balances[to] += value;
-        balances[from] -= value;
-        emit Transfer(from, to, value);
-        return true;   
-       
-    
-    }
-    
-    
-        
+    function balanceOf(address owner) public returns(uint) {
+        return balances[owner];
     }
     
     function transfer(address to, uint value) public returns(bool) {
         require(balanceOf(msg.sender) >= value, 'balance too low');
         balances[to] += value;
+        balances[msg.sender] -= value;
+       emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint value) public returns(bool) {
+        require(balanceOf(from) >= value, 'balance too low');
+        require(allowance[from][msg.sender] >= value, 'allowance too low');
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;   
     }
     
     function approve(address spender, uint value) public returns (bool) {
@@ -44,4 +41,4 @@ contract Token {
         emit Approval(msg.sender, spender, value);
         return true;   
     }
-}
+}  
